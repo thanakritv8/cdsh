55da170a355c95644c212fdf2d807efd6769863c
Thanakrit <thanakritv8@gmail.com>
2018-08-14 23:06:23 +0700

    create by thung

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,20 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { GooglePlus } from '@ionic-native/google-plus';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import firebase from 'firebase';
+
+export const firebaseConfig={
+  apiKey: "AIzaSyAMPj_B78mGX8e8Hy7KfOdzDJPMbH5N9C8",
+  authDomain: "cdsh-37b7f.firebaseapp.com",
+  databaseURL: "https://cdsh-37b7f.firebaseio.com",
+  projectId: "cdsh-37b7f",
+  storageBucket: "cdsh-37b7f.appspot.com",
+  messagingSenderId: "1063869344462"
+}
+firebase.initializeApp(firebaseConfig)
 
 @NgModule({
   declarations: [
@@ -21,7 +35,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -34,7 +50,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    GooglePlus
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase} from 'angularfire2/database';
+import { GooglePlus } from '@ionic-native/google-plus';
+import firebase from 'firebase';
+import 'rxjs';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,43 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public googleplus: GooglePlus, private fdb: AngularFireDatabase) {
+    this.navCtrl = navCtrl;
+    this.fdb.list("/Thung/").subscribe(_data => {
+      this.arrData = _data;
 
+      console.log(this.arrData);
+    });
+  }
+
+  myIcon: string = "bulb";
+  myIconColor: string = "yellow";
+  myTitleColor: string = "dark";
+
+  arrData = [];
+  toggleUpdate(item) {  
+    const items = this.fdb.list('/Thung');
+    items.update(item.$key, { status: item.status });
+  }
+
+  statusLogin: boolean = false;
+  email: string = "";
+  userId: string = "";
+  login(){
+    this.googleplus.login({
+      'webClientId':'1063869344462-6rc2rsagqsl2vnqmoqenlefacibs2agn.apps.googleusercontent.com',
+      'offline':true
+    }).then(res=>{
+      firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken)).then(user=>{
+        this.statusLogin = true;
+        this.email = user.email;
+        this.userId = user.uid;
+        alert("Login SUCCESS" + user.uid)
+      })
+    }).catch(ns=>{
+      this.statusLogin = false;
+      alert("NOT SUCCESS")
+    })
   }
 
 }
